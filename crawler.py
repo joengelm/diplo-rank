67fed77c8fe2ea19f6983228672f4f4275e827f4
joengelm <joseph_engelman@brown.edu>
2017-02-04 18:57:52 -0500

    more sql

--- crawler.py
+++ crawler.py
@@ -18,22 +18,29 @@ CLIENT_ID = '49009eb8904b11a2a5d2c6bdc162dd32'
 
 USAGE = 'Usage: python3 crawler.py <initial_track_url> [-l <traversal_limit>]'
 
+# TABLE CREATION
 CREATE_USER_TABLE = '''CREATE TABLE IF NOT EXISTS users (
     id integer PRIMARY KEY, username text, url text, avatar_url text, 
     country text, city text, description text, 
-    track_count integer, follow_count integer, following_count integer )'''
-
+    track_count integer, followers_count integer, followings_count integer )'''
 CREATE_FOLLOWING_TABLE = '''CREATE TABLE IF NOT EXISTS following (
     id integer, following_id integer )'''
-
 CREATE_COMMENTS_TABLE = '''CREATE TABLE IF NOT EXISTS comments (
     id integer, target_id integer )'''
-
 CREATE_LIKES_TABLE = '''CREATE TABLE IF NOT EXISTS likes (
     id integer, target_id integer )'''
-
 CREATE_REPOSTS_TABLE = '''CREATE TABLE IF NOT EXISTS reposts (
     id integer, target_id integer )'''
+
+# INSERTIONS
+INSERT_USER = '''INSERT OR REPLACE INTO users VALUES (
+    id=:id, user=:username, url=:permalink_url, avatar_url=:avatar_url,
+    country=:country, city=:city, description=:description,
+    track_count=:track_count, followers_count=:followers_count, followings_count=:followings_count )'''
+INSERT_FOLLOWING = '''INSERT INTO following VALUES (?,?)'''
+INSERT_COMMENT = '''INSERT INTO comments VALUES (?,?)'''
+INSERT_LIKE = '''INSERT INTO likes VALUES (?,?)'''
+INSERT_REPOST = '''INSERT INTO reposts VALUES (?,?)'''
 
 logging.basicConfig(level=logging.WARNING, format='%(message)s')
 
@@ -78,11 +85,20 @@ class Crawler:
             user_data = self.user_data_queue.get()
             try:
                 user = user_data['user']
-                following = user_data['following']
+                self.conn.execute(INSERT_USER, user.fields())
+
+                followings = user_data['followings']
+                self.conn.executemany(INSERT_FOLLOWING, [(user.id, following.id) for following in followings])
+
                 comments = user_data['comments']
+                self.conn.executemany(INSERT_COMMENT, [(user.id, commentee.id) for commentee in comments])
+
                 likes = user_data['likes']
+                self.conn.executemany(INSERT_LIKE, likes)
+
                 reposts = user_data['reposts']
-                self.conn.execute("INSERT OR REPLACE INTO tracks VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", track_details)
+                self.conn.executemany(INSERT_REPOST, reposts)
+                
                 self.conn.commit()
                 logging.warning("{:1.3f}  {} ({})".format(track.favoritings_count / track.playback_count, track.title, track.permalink_url))
             except:
@@ -91,7 +107,6 @@ class Crawler:
 
     def scraper(self):
         try:
-
             user_id = self.user_id_queue.get()
             path = '/users/' + str(user_id)
 
@@ -101,7 +116,7 @@ class Crawler:
             comments = client.get(path + '/comments')
             reposts = client.get('/e1' + path + '/track_reposts')
             #play_count = get_play_count(user_id)
-            obj = {
+            user_data = {
                 'user': user,
                 'followings': followings,
                 'likes': likes,
@@ -109,8 +124,7 @@ class Crawler:
                 'reposts': reposts 
             }
             
-            self.user_data_queue.put(obj)
-
+            self.user_data_queue.put(user_data)
         except:
             print('Error')
 
